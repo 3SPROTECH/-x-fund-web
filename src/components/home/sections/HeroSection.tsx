5feadce6dfb678d477e4cb7dbba36fa4552cd07b
Oussama Idsaid <ossamaidsaid@3sprotech.com>
2026-02-02 11:27:36 +0100

    feat: configuration des styles globaux (v4), polices et alias d'importation

--- src/components/home/sections/HeroSection.tsx
+++ src/components/home/sections/HeroSection.tsx
@@ -1,8 +1,8 @@
-import Section from "../../ui/Section"
+import Section from "@/components/ui/Section"
 
 const HeroSection = () => {
   return (
-    <Section className="bg-red-500">HeroSection</Section>
+    <Section className="bg-primary">HeroSection</Section>
   )
 }
 
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-import HeroSection from "../components/home/sections/HeroSection"
+import HeroSection from "@/components/home/sections/HeroSection"
 
 const HomePage = () => {
   return (
